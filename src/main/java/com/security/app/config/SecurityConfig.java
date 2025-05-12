cfbb9dd285117d61b8fb53da1e54c98ddc3d6c24
NithinU2802 <nithinu2810@gmail.com>
2025-05-13 00:16:20 +0530

    database configuration

--- src/main/java/com/security/app/config/SecurityConfig.java
+++ src/main/java/com/security/app/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.security.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -18,8 +19,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authz ->
-                authz.requestMatchers("/api/users/**").authenticated()
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/profile"))
+                .authorizeHttpRequests(authz ->
+                authz
+                        .requestMatchers("/api/users/**").authenticated()
+                        .requestMatchers(HttpMethod.POST,"/profile").permitAll()
                         .requestMatchers("/").permitAll()
                         .anyRequest().permitAll()
                 )
