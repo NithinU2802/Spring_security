2d08645fa889cb4be8d4c49b1b237b13783ad9b5
NithinU2802 <nithinu2810@gmail.com>
2025-05-13 14:05:32 +0530

    Generation and Validation with Jwt

--- src/main/java/com/security/app/config/SecurityConfig.java
+++ src/main/java/com/security/app/config/SecurityConfig.java
@@ -1,36 +1,49 @@
 package com.security.app.config;
 
+import com.security.app.security.JwtFilter;
 import com.security.app.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/profile"))
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/login"))
                 .authorizeHttpRequests(authz ->
                 authz
                         .requestMatchers(HttpMethod.POST,"/profile").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
                         .requestMatchers("/api/users/**").authenticated()
                         .requestMatchers("/").permitAll()
                         .anyRequest().permitAll()
                 )
-                .formLogin(form -> form.permitAll().defaultSuccessUrl("/panel"));
+//                .formLogin(form -> form.permitAll().defaultSuccessUrl("/panel"))    // login form
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // To avoid session
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                ;
 
         return http.build();
     }
@@ -60,6 +73,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        return new ProviderManager(List.of(authenticationProvider()));
     }
 
 
